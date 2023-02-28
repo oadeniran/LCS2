2b7e3026003bd76150d93de5d0c94b2147b2571e
oadeniran <60529828+oadeniran@users.noreply.github.com>
2023-02-28 12:48:17 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,8 +4,14 @@ def func(x):
 def func2(x):
   return x + 2
 
+def func3(x):
+  return x + 3
+
 def test_func():
   assert func(3) == 4
 
 def test_func2():
   assert func2(3) == 5
+  
+def test_func3():
+  assert func3(3) == 6
